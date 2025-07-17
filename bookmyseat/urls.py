1b0d3215fb2870c8695a03536bc53385b840a093
mohanbhimaneni <bhmohan01@gmail.com>
2025-07-17 16:11:33 +0530

    Changes made to urls.py to ensure media are serving

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('',include('users.urls')),
+    path('', include('users.urls')),
     path('movies/', include('movies.urls')),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ]
-
+# Ensure DEBUG is True for local media serving
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
